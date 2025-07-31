ead91d2ad678f681abfce95d5fbbf68195359e67
tharushi330 <tharushiwarangana78@gmail.com>
2025-07-31 09:50:45 +0530

    Create Food_item.dart

---
+++ lib/models/food_item.dart
@@ -0,0 +1,13 @@
+class FoodItem {
+  final String image;
+  final String name;
+  final double price;
+  final double rating;
+
+  FoodItem({
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.rating,
+  });
+}
